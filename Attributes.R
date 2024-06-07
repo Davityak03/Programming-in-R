a3c0cd1fe9403564db95bbe22a8ce9aedea61acc
Davitya Kumar <121346374+Davityak03@users.noreply.github.com>
2024-06-07 21:15:55 +0530

    Add files via upload

---
+++ Attributes.R
@@ -0,0 +1,8 @@
+#Attributes 
+#R objects have attributes(metadata for object)
+#Examples of R object attributes(names, dimnames, dimensions[matrices,arrays]
+#,class[Integer,numeric],length, other user defined attributes and meta data)
+x<-1
+x
+#not all objects contain attributes, in which case the attributes function returns NULL
+attributes(x)
