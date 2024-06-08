13a336a3ccbc2d661dcf214bca036ac8a3e87eeb
Davitya Kumar <121346374+Davityak03@users.noreply.github.com>
2024-06-08 19:47:30 +0530

    Add files via upload

---
+++ print formatting.R
@@ -0,0 +1,11 @@
+#there are different ways to print values in R 
+#normal print
+x<-("Hello World")
+print(x)
+
+#paste()
+print(paste("hello","word"))
+print(paste("hello","word",sep="-"))
+
+#paste0()
+print(paste0("hello","world"))#i concatenate the values and print them
