71b12e64fa9182aa0df7b48244d2e0a14f1bf100
DavityaKumar <davityak09@gmail.com>
2025-01-14 17:59:05 +0530

    update the file

--- Polynomial_Rgression.R
+++ Polynomial_Rgression.R
@@ -1,13 +1,25 @@
 #polynomial regression in R
 #Polynomial regression is a type of regression analysis in which the relationship between the independent variable (predictor) and the dependent variable (response) is modeled as an n-th degree polynomial.
 
-data(airquality)
-head(airquality)
+# Load necessary library
+library(ggplot2)
 
-airquality_clean<-na.omit(airquality)
+# Load the iris dataset
+data(iris)
 
-model1<-lm(airquality_clean$Temp~airquality_clean$Wind)
-summary(model1)
-plot(airquality_clean$Temp~airquality_clean$Wind)
-abline(model1,lwd=3,col="red")
+# Fit a polynomial regression model with degree 2 (quadratic)
+model <- lm(Sepal.Length ~ poly(Petal.Length, 2), data = iris)
+
+# Summarize the model
+summary(model)
+
+# Predict using the model
+iris$predicted_sepal_length <- predict(model, newdata = iris)
+
+# Plot the data and the fitted polynomial regression curve
+ggplot(iris, aes(x = Petal.Length, y = Sepal.Length)) +
+  geom_point() + 
+  geom_line(aes(x = Petal.Length, y = predicted_sepal_length), color = "blue") +
+  labs(title = "Polynomial Regression (Degree 2)", x = "Petal Length", y = "Sepal Length")
+
 
